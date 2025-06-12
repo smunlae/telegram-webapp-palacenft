3bf37918454bcf6df3e466446bc431c6fc85f66c
smunlae <69656830+smunlae@users.noreply.github.com>
2025-06-12 03:15:11 +0200

    replaced link to https://telegram-webapp-palacenft-production.up.railway.app/

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ bot = telebot.TeleBot("7876485835:AAGz0RqhyobWjAi7g7KHl8r8mn-2hVwhb2E")
 @bot.message_handler(commands=["start"])
 def start(msg):
     kb = ReplyKeyboardMarkup(resize_keyboard=True)
-    kb.add(KeyboardButton("📊 Открыть Market", web_app=WebAppInfo(url="https://yourapp.up.railway.app")))
+    kb.add(KeyboardButton("📊 Открыть Market", web_app=WebAppInfo(url="https://telegram-webapp-palacenft-production.up.railway.app")))
     bot.send_message(msg.chat.id, "Добро пожаловать! Нажми кнопку ниже, чтобы открыть маркет:", reply_markup=kb)
 
 bot.polling()
